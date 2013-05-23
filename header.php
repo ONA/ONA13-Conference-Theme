57d13eb6dc61b476b802c79bf4493001f682f89c
Daniel Bachhuber <d@danielbachhuber.com>
2013-05-23 12:49:57 -0700

    Turn our functions.php into a child theme functions.php too

--- header.php
+++ header.php
@@ -30,7 +30,6 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/isotope/jquery.isotope.min.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
