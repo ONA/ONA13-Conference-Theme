cefce24912abe1b7ff31012758edb8f6c85e7a25
Adam <anekola@gmail.com>
2013-08-20 19:58:00 -0400

    Adding social media buttons to the bottom of posts and sessions
    They just looked so bad at the top

--- single.php
+++ single.php
@@ -42,6 +42,18 @@ get_header(); ?>
                     <? } ?>
                     <div class="entry-content">
                         <? the_content();?>
+                        <!-- AddThis Button BEGIN -->
+                        <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+                        <a class="addthis_button_preferred_1"></a>
+                        <a class="addthis_button_preferred_2"></a>
+                        <a class="addthis_button_preferred_3"></a>
+                        <a class="addthis_button_preferred_4"></a>
+                        <a class="addthis_button_compact"></a>
+                        <a class="addthis_counter addthis_bubble_style"></a>
+                        </div>
+                        <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+                        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=onlinenewsassociation"></script>
+                        <!-- AddThis Button END -->
                         <? ONA_display_related_by_tag(); ?>
                     </div>
                     <footer class="entry-meta">
