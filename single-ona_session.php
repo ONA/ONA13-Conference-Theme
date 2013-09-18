b1042dbf33b7afa9533a4b5731ab9feaf65d14bb
Adam <anekola@gmail.com>
2013-09-17 23:12:23 -0400

    Session template, including speakers and some other info

--- single-ona_session.php
+++ single-ona_session.php
@@ -12,12 +12,12 @@
                     </header>
                     <ul class="session-meta">
                     	<li class="day"><?php echo $session->get_start_time( 'l - g:i A' ); ?></li>
-                        <li class="room"><?php echo $session->get_room_name(); ?></li>
-                        <li class="track type1"><?php echo $session->get_session_type_name(); ?></li>
-                        <li class="hash"><?php echo ( $session->get_hashtag() ) ? $session->get_hashtag() : '<em>None</em>'; ?></li>
+                        <li class="room"><?php echo ( $session->get_room_name() ) ? $session->get_room_name() : '<em>No Room</em>'; ?></li>
+                        <li class="track"><div class="<?php echo $session->get_session_type_name();?>"><?php echo ( $session->get_session_type_name() ) ? $session->get_session_type_name() : 'Other'; ?></div></li>
+                        <li class="hash"><?php echo ( $session->get_hashtag() ) ? $session->get_hashtag() : '<em>No Hashtag</em>'; ?></li>
                     </ul>
                     <div class="entry-content">
-                    	<div class="presenters"></div>
+                    	
                         <?php $sponsor = get_post_meta( get_the_ID(), '_assigned_sponsor' );
 						if ( $sponsor[0] != "") { 
 							$sponsor_name = get_the_title($sponsor[0]); 
@@ -33,14 +33,21 @@
                         </div>
                         <?php } ?>
                         <p><?php echo $session->get_description();?></p>
-                        <div class="resources"></div>
-                        <div class="liveblogging">
-                        <? 
-						$url = 'https://www.rebelmouse.com/static/js-build/embed/embed.js?site=nekolaweb2%2Fdfsdfsf&height=1500&flexible=1';
-print_r(get_headers($url, 1));
-?>
-                        <script type="text/javascript" class="rebelmouse-embed-script" src="https://www.rebelmouse.com/static/js-build/embed/embed.js?site=nekolaweb2%2Fsdsdgsg&height=1500&flexible=1"></script>
+                        <?php $speakers = $session->get_speakers(); 
+						if ($speakers) { ?>
+                    	<div class="speakers">
+                        	<h4>Speakers</h4>
+							<?php foreach ($speakers as $speaker){ ?>
+                            <p><?php echo $speaker;?></p>
+                            <?php } ?>
                         </div>
+                        <?php } ?>
+                        <?php $rebelmouse = $session->get_rebelmouse(); 
+						if ($rebelmouse) { ?>
+                        <div class="liveblogging">
+                        <script type="text/javascript" class="rebelmouse-embed-script" src="https://www.rebelmouse.com/static/js-build/embed/embed.js?site=ona13%2F<?php echo $rebelmouse;?>&height=1500&flexible=1"></script>
+                        </div>
+                        <?php } ?>
                         <?php ONA_display_related_by_tag(); ?>
                         <!-- AddThis Button BEGIN -->
                         <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
