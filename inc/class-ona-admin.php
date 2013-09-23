1b16a94a1230d916725954f43fc51f6ae9aa2600
Daniel Bachhuber <d@danielbachhuber.com>
2013-09-23 15:47:43 -0700

    Allow the speaker importer to be run from the admin bar

--- inc/class-ona-admin.php
+++ inc/class-ona-admin.php
@@ -7,7 +7,8 @@ class ONA_Admin {
 	function __construct() {
 
 		add_action( 'admin_bar_menu', array( $this, 'action_admin_bar_menu' ), 99 );
-		add_action( 'wp_ajax_ona_run_session_importer', array( $this, 'handle_ajax_run_importer' ) );
+		add_action( 'wp_ajax_ona_run_speaker_importer', array( $this, 'handle_ajax_run_speaker_importer' ) );
+		add_action( 'wp_ajax_ona_run_session_importer', array( $this, 'handle_ajax_run_session_importer' ) );
 
 	}
 
@@ -27,6 +28,20 @@ class ONA_Admin {
 		$wp_admin_bar->add_node( $args );
 
 		$query_args = array(
+			'action' => 'ona_run_speaker_importer',
+			'nonce'  => wp_create_nonce( 'ona-speaker-importer' ),
+			);
+		$href = add_query_arg( $query_args, admin_url( 'admin-ajax.php' ) );
+
+		$args = array(
+			'id'      => 'ona-speaker-importer',
+			'title'   => "Import Speakers",
+			'href'    => $href,
+			'parent'  => 'ona-importer',
+			);
+		$wp_admin_bar->add_node( $args );
+
+		$query_args = array(
 			'action' => 'ona_run_session_importer',
 			'nonce'  => wp_create_nonce( 'ona-session-importer' ),
 			);
@@ -42,9 +57,22 @@ class ONA_Admin {
 	}
 
 	/**
-	 * Handle an AJAX request to run the importer
+	 * Handle an AJAX request to run the speaker importer
 	 */
-	public function handle_ajax_run_importer() {
+	public function handle_ajax_run_speaker_importer() {
+
+		if ( ! wp_verify_nonce( $_GET['nonce'], 'ona-speaker-importer' )
+		|| ! current_user_can( 'manage_options' ) )
+			wp_die( "You shouldn't be here..." );
+
+		ONA13_Importer::import_speakers( 'https://docs.google.com/spreadsheet/pub?key=0AgtyAD_1PrgVdHdtZnRnTDR0Wk53eElicUMyRnZTNFE&single=true&gid=0&output=csv' , array( $this, 'output') );
+		exit;
+	}
+
+	/**
+	 * Handle an AJAX request to run the session importer
+	 */
+	public function handle_ajax_run_session_importer() {
 
 		if ( ! wp_verify_nonce( $_GET['nonce'], 'ona-session-importer' )
 		|| ! current_user_can( 'manage_options' ) )
