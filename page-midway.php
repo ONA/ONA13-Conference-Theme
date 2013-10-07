14fc1a714c479518ce73ed3e73d4984cf6ceefbf
Adam <anekola@gmail.com>
2013-10-06 23:15:12 -0400

    Midway - added a dropdown menu

--- page-midway.php
+++ page-midway.php
@@ -52,6 +52,11 @@ get_header(); ?>
 		</div><!-- .entry-content -->
 
 		<div id="participants">
+        
+        <select name="participants">
+        	<option>Find a Midway participant</option>
+        
+        
 		<?	$args = array (
 				'post_type'=> 'sponsors',
 				'post_status'  => 'publish',
@@ -69,39 +74,25 @@ get_header(); ?>
 			 ); 
 			$my_query = new WP_Query($args);
 			if ($my_query->have_posts()) { 
-			
-				echo '<h2 class="sponsor_level">'.$level.'</h2>';
+				
 				
 				while ( $my_query->have_posts() ) : $my_query->the_post(); 
 					$external_link = get_post_meta( get_the_ID(), '_sponsor_url', true );
-					$sponsor_level = get_post_meta( get_the_ID(), '_sponsor_level', true );?>
-	
-				<!--<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					
-					<header class="entry-header">
-						<h1 class="entry-title"><a href="<?php the_permalink(); ?> "><?php the_title(); ?></a></h1>
-					</header>
-			
-					<div class="entry-content">
-						<?php if ( has_post_thumbnail() ) { 
-							 echo '<a href="'.$external_link.'" target="_blank">';
-							 echo the_post_thumbnail( 'medium' );
-							 echo '</a>'; 
-						} 
-						the_content(); ?>
-						<p><a href="<?php echo $external_link;?>"  target='_blank'><?php echo $external_link;?></a></p>
-					</div>
-				</article>-->	
-
-				<?php if ( has_post_thumbnail() ) { 
+					echo '<option value="">'.get_the_title().'</option>';
+				endwhile;
+				echo '</select>';
+				
+				while ( $my_query->have_posts() ) : $my_query->the_post(); 
+					$external_link = get_post_meta( get_the_ID(), '_sponsor_url', true );
+					if ( has_post_thumbnail() ) { 
 						 echo '<a href="'.$external_link.'" target="_blank">';
 						 echo the_post_thumbnail( 'medium' );
 						 echo '</a>'; 
-					} ?>
-
-			<?php 	wp_reset_postdata();
-				endwhile;
-			} ?>
+					} 				
+				endwhile; ?>
+				
+				
+			<? wp_reset_postdata(); } ?>
 			</div>
 
 	</article><!-- #post -->
