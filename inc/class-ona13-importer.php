86b3344138becfbcc24d1833b5c5218ef6ca48cd
Connor Jennings <cojennin@gmail.com>
2013-10-12 14:54:36 -0400

    Haven't fully tested, but this should fix issue with html importing.

--- inc/class-ona13-importer.php
+++ inc/class-ona13-importer.php
@@ -24,7 +24,6 @@ class ONA13_Importer {
 			array(
 					'csv_field'          => 'Session Description',
 					'object_field'       => 'description',
-					'sanitize_callback'  => 'strip_tags',
 				),
 			array(
 					'csv_field'          => 'Room',
