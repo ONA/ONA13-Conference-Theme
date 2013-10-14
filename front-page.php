4af9822f27e1b89f9eb1b03a287ddd0ec61b56dd
Adam <anekola@gmail.com>
2013-10-14 16:35:02 -0400

    New homepage layout

--- front-page.php
+++ front-page.php
@@ -9,65 +9,25 @@
 get_header(); ?>
 
 	<div id="primary" class="site-content">
-		<div id="content" role="main">
-        
-        <!-- Main Info -->
-        <div class="lead_package">
-        	<div class="lead_text">
-            	<p>The Online News Association 2013 Conference & Awards Banquet is the premier gathering of highly engaged digital journalists shaping media now. Learn about new tools and technologies, network with peers from around the world and celebrate excellence at the Online Journalism Awards.</p>
-                <a href="<?php echo home_url(); ?>/register/"><div class="button">Register today</div></a>
-            </div>
-            <ul class="headlines">
-            	<li class="title"><a href="<?php echo home_url(); ?>/category/headlines/">Recent Headlines</a></li>
-            <?php $category_id = get_cat_ID('Headlines');
-				$args = array(
-				'numberposts' => 3,
-				'category' => $category_id,
-				'orderby' => 'post_date',
-				'order' => 'DESC',
-				'post_type' => 'post',
-				'post_status' => 'publish');
-			
-				$recent_posts = wp_get_recent_posts( $args );
-				foreach( $recent_posts as $recent ){
-					$category = get_the_category($recent["ID"]); 
-					$sponsor = get_post_meta( $recent["ID"], '_assigned_sponsor' );
-					echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="'.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'<br/>';
-					echo '<p class="date">'.get_the_time('M j, Y', $recent["ID"]);
-					echo ' | <span>'.$category[0]->cat_name.'</span>';
-					if(is_numeric($sponsor[0])) {
-						echo '<span class="Sponsored">Sponsored</span>';	
-					}
-					echo '</p>';
-					echo '</a></li> ';
-				}
-			?>
-            	<li class="more"><a href="<?php echo home_url(); ?>/category/headlines/">More headlines &rarr;</a></li>
-            </ul>
-        </div>
-        
-        <!-- Conference Details -->
-        <div>
-        	<?php dynamic_sidebar( 'row1' ); ?>
-        </div>
-        
-        
-        <!-- Participate -->
-        <div>
-        	<h3><span>Participate</span></h3>
-            <?php dynamic_sidebar( 'row2' ); ?>
-        </div>
-        
-        <!-- Sponsors -->
-        <div class="sponsor-row">
-        	<h3><span>Sponsors</span></h3>
-            <div class="logos">
-                <?php dynamic_sidebar( 'sponsors' ); ?>
-            </div>
-        	<div><p>Sponsorships are still available for ONA13. <a href="/sponsor/sponsor-packages/">Learn more &rarr;</a></p></div>
-        </div>
-
-		</div><!-- #content -->
+	
+    	<div id="my-timeline"></div>
+    
 	</div><!-- #primary -->
 
+	<script type="text/javascript">
+        var timeline_config = {
+            width:              '100%',
+            height:             '600',
+            source:             'https://docs.google.com/spreadsheet/pub?key=0AliJph1LBLsOdEtSdTBhd3ltMjY3bHhjNUJrTl9xUlE&output=html',
+            embed_id:           'my-timeline',               //OPTIONAL USE A DIFFERENT DIV ID FOR EMBED
+            start_at_end:       true,                          //OPTIONAL START AT LATEST DATE
+            font:               'Bevan-PotanoSans',             //OPTIONAL FONT
+            maptype:            'watercolor',                   //OPTIONAL MAP STYLE
+            css:                '<?php echo get_stylesheet_directory_uri();?>/css/timeline.css',     //OPTIONAL PATH TO CSS
+            js:                 '<?php echo get_stylesheet_directory_uri();?>/js/timeline-min.js'    //OPTIONAL PATH TO JS
+        }
+    </script>
+    
+    <script type="text/javascript" class="rebelmouse-embed-script" src="https://www.rebelmouse.com/static/js-build/embed/embed.js?site=ona13_broadcast_for_all&height=1500&flexible=1&theme=4880"></script>
+    
 <?php get_footer(); ?>
